9b8908d5302e22946d955067f8adb28386856e49
Max Rykin <max.rykin@powerlinks.com>
2018-10-08 17:44:41 -0400

    Read input from stdin; Buffer creation fix

--- src/main/java/bulkload/BulkLoad.java
+++ src/main/java/bulkload/BulkLoad.java
@@ -15,7 +15,7 @@ package bulkload;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.lang.System;
