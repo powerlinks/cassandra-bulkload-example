17cc4e30764f4d90c75b87a8cd419f738b1dea7b
Max Rykin <max.rykin@powerlinks.com>
2019-10-24 17:51:58 +0100

    Add region parameter to the loader execution command

--- iterator.sh
+++ iterator.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 y="2019"
 m="09"
+region=$(curl -H "Metadata:true" "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-08-01&format=text")
 
 for d in {25..16}
 do
     for h in {00..23}
         do
-           bash loader.sh  sync/year=$y/month=$m/day=$d/hour=$h/*.gz
+           bash loader.sh  sync/year=$y/month=$m/day=$d/hour=$h/*.gz $region
         done
 done
